7889ca26d0acb734866797112d75d86dbba4c578
RynoCODE <rohit.roychoudhury13@gmail.com>
2024-01-22 20:20:36 +0530

    migrate

--- codeshetra/settings.py
+++ codeshetra/settings.py
@@ -113,7 +113,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = 'static/'
+STATIC_URL = 'public/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static'),
+    os.path.join(BASE_DIR, 'public'),
 ]
