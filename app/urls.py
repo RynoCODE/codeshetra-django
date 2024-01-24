bb745e8e43d630fd5869baa43ac604e857534536
RynoCODE <rohit.roychoudhury13@gmail.com>
2024-01-24 18:23:41 +0530

    bug fix

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signin', views.login, name='signin'),
     path('signup', views.signup, name='signup'),
     path('signout', views.signout, name='signout'),
 ]
